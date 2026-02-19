9866720583c9af18008d368d953fc7b594656df6
MasterYoav <realyoavperetz@gmail.com>
2026-02-19 06:32:32 +0200

    Fix Vercel build: render only Hero on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
 import Hero from "@/components/Hero";
-import ProjectsSection from "@/components/ProjectsModal";
-import ContactSection from "@/components/ContactModal";
-import ProjectsModal from "@/components/ProjectsModal";
 
-export default function Page() {
-  return (
-    <>
-      <Hero />
-
-      <section id="projects">
-        <ProjectsModal />
-      </section>
-
-      <section id="contact">
-        <ContactSection />
-      </section>
-    </>
-  );
+export default function Home() {
+  return <Hero />;
 }
